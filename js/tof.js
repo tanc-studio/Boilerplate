23f0a595550d0d026d43094763bbc58775bd6fb8
Tanc <tanc.developer@gmail.com>
2023-11-05 19:12:05 +0000

    first commit

---
+++ js/tof.js
@@ -0,0 +1,41 @@
+// Navigation
+
+var toggle = document.getElementById("darkmodeToggle");
+
+var storedTheme = localStorage.getItem('theme') || (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
+if (storedTheme)
+    document.documentElement.setAttribute('data-theme', storedTheme)
+
+
+toggle.onclick = function() {
+    var currentTheme = document.documentElement.getAttribute("data-theme");
+    var targetTheme = "light";
+
+    if (currentTheme === "light") {
+        targetTheme = "dark";
+    }
+
+    document.documentElement.setAttribute('data-theme', targetTheme)
+    localStorage.setItem('theme', targetTheme);
+};
+
+
+// Navigation
+
+
+
+var isNavOpen = false;
+
+function toggleNav() {
+  var toggleBtn = document.getElementById("tofBurger");
+  var sidenav = document.getElementById("tofSideNav");
+  if (isNavOpen) {
+    sidenav.classList.remove("open");
+    toggleBtn.classList.remove("open");
+    isNavOpen = false;
+  } else {
+    sidenav.classList.add("open");
+    toggleBtn.classList.add("open");
+    isNavOpen = true;
+  }
+}
